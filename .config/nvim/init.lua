3b3a84b41b1f0658548024984d6e6688c48c3a38
JasonPekos <jason.pekos@gmail.com>
2024-10-24 20:46:55 -0400

    tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -418,7 +418,7 @@ require('lazy').setup({
     config = function()
       vim.g.slime_target = 'tmux'
       vim.g.slime_paste_file = vim.fn.tempname()
-      vim.g.slime_default_config = { socket_name = 'default', target_pane = '{last}' }
+      vim.g.slime_default_config = { socket_name = 'default', target_pane = '{top-right}' }
       vim.g.slime_dont_ask_default = 1
     end,
   },
